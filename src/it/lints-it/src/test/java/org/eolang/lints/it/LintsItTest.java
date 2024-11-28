587174b77b8de879b32c64a5169dea8da995fd75
h1alexbel <h1alexbelx@gmail.com>
2024-11-28 19:30:24 +0300

    feat(#59): case

--- src/it/lints-it/src/test/java/org/eolang/lints/it/LintsItTest.java
+++ src/it/lints-it/src/test/java/org/eolang/lints/it/LintsItTest.java
@@ -1,9 +1,20 @@
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
 import org.junit.jupiter.api.Test;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 
 final class LintsItTest {
 
     @Test
     void runs() {
-
+        final XML xmir = new XMLDocument("<program/>");
+        for (final Lint lint : new XslLints()) {
+            MatcherAssert.assertThat(
+                "passes with no exceptions",
+                lint.defects(xmir),
+                Matchers.notNullValue()
+            );
+        }
     }
 }
