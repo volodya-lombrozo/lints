49abdb702a0bb1747982cbf85dc3caffe8dc7508
h1alexbel <h1alexbelx@gmail.com>
2024-11-28 21:04:31 +0300

    feat(#59): reproduced

--- src/it/lints-it/src/test/java/org/eolang/lints/it/LintsItTest.java
+++ src/it/lints-it/src/test/java/org/eolang/lints/it/LintsItTest.java
@@ -3,11 +3,14 @@ import com.jcabi.xml.XMLDocument;
 import org.junit.jupiter.api.Test;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.eolang.lints.Lint;
+import org.eolang.lints.XslLints;
+import java.io.IOException;
 
 final class LintsItTest {
 
     @Test
-    void runs() {
+    void lintsProgram() throws IOException {
         final XML xmir = new XMLDocument("<program/>");
         for (final Lint lint : new XslLints()) {
             MatcherAssert.assertThat(
