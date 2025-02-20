8e4884085b1dd018fceeaec243947e27bc04d4dd
h1alexbel <h1alexbelx@gmail.com>
2025-02-20 19:45:04 +0300

    feat(#259): test

---
+++ src/test/java/org/eolang/lints/LtInconsistentArgsTest.java
@@ -0,0 +1,41 @@
+package org.eolang.lints;
+
+import java.io.IOException;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
+import org.eolang.parser.EoSyntax;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link LtInconsistentArgs}.
+ *
+ * @since 0.41.0
+ */
+final class LtInconsistentArgsTest {
+
+    @Test
+    void catchesArgumentsInconsistency() throws IOException {
+        MatcherAssert.assertThat(
+            "Defects are empty, but they should not",
+            new LtInconsistentArgs().defects(
+                new MapOf<>(
+                    new MapEntry<>(
+                        "foo",
+                        new EoSyntax(
+                            String.join(
+                                "\n",
+                                "# Foo",
+                                "[] > foo",
+                                "  bar 42 > x",
+                                "  bar 1 2 3 > y"
+                            )
+                        ).parsed()
+                    )
+                )
+            ),
+            Matchers.hasSize(Matchers.greaterThan(0))
+        );
+    }
+}
