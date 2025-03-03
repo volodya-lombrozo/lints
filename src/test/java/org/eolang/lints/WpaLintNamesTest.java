93a9094f29c65f8ac18f4305e54f93556e16abe5
h1alexbel <h1alexbelx@gmail.com>
2025-03-03 16:34:09 +0300

    bug(#385): archunit tests

---
+++ src/test/java/org/eolang/lints/WpaLintNamesTest.java
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package org.eolang.lints;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link WpaLintNames}.
+ *
+ * @since 0.0.43
+ */
+final class WpaLintNamesTest {
+
+    @Test
+    void staysPackagePrivate() {
+        ArchRuleDefinition.classes()
+            .that().haveSimpleName("WpaLintNames")
+            .should().bePackagePrivate()
+            .check(new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("org.eolang.lints")
+            );
+    }
+}
