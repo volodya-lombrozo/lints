51214acdfecf0e4489ddca63cfac13fb58406b47
h1alexbel <h1alexbelx@gmail.com>
2025-03-18 18:44:53 +0300

    feat(#36): clean for qulice

--- src/main/java/org/eolang/lints/LtIncorrectNumberOfAttrs.java
+++ src/main/java/org/eolang/lints/LtIncorrectNumberOfAttrs.java
@@ -13,13 +13,12 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import org.cactoos.list.ListOf;
 
 /**
  * Lint to check incorrect number of attributes passed to the object in scope.
+ *
  * @since 0.0.43
  */
 final class LtIncorrectNumberOfAttrs implements Lint<Map<String, XML>> {
@@ -28,37 +27,29 @@ final class LtIncorrectNumberOfAttrs implements Lint<Map<String, XML>> {
     public Collection<Defect> defects(final Map<String, XML> pkg) throws IOException {
         final Collection<Defect> defects = new LinkedList<>();
         final Map<String, Integer> definitions = LtIncorrectNumberOfAttrs.objectDefinitions(pkg);
-        // step 2: scan usages
         pkg.forEach(
-            new BiConsumer<String, XML>() {
-                @Override
-                public void accept(final String program, final XML xmir) {
-                    new Xnav(xmir.inner()).path("//o[@base and not(@base='∅')]").forEach(
-                        new Consumer<Xnav>() {
-                            @Override
-                            public void accept(final Xnav xnav) {
-                                final int provided = (int) xnav.elements(Filter.withName("o")).count();
-                                final String object = xnav.attribute("base").text().orElse("unknown");
-                                final Integer expected = definitions.get(object);
-                                if (expected != null && provided != expected) {
-                                    defects.add(
-                                        new Defect.Default(
-                                            LtIncorrectNumberOfAttrs.this.name(),
-                                            Severity.ERROR,
-                                            program,
-                                            Integer.parseInt(xnav.attribute("line").text().orElse("0")),
-                                            String.format(
-                                                "The object %s expects %d arguments, while %d provided",
-                                                object, expected, provided
-                                            )
-                                        )
-                                    );
-                                }
-                            }
+            (program, xmir) -> new Xnav(xmir.inner()).path("//o[@base and not(@base='∅')]")
+                .forEach(
+                    xnav -> {
+                        final int provided = (int) xnav.elements(Filter.withName("o")).count();
+                        final String object = xnav.attribute("base").text().orElse("unknown");
+                        final Integer expected = definitions.get(object);
+                        if (expected != null && provided != expected) {
+                            defects.add(
+                                new Defect.Default(
+                                    this.name(),
+                                    Severity.ERROR,
+                                    program,
+                                    Integer.parseInt(xnav.attribute("line").text().orElse("0")),
+                                    String.format(
+                                        "The object %s expects %d arguments, while %d provided",
+                                        object, expected, provided
+                                    )
+                                )
+                            );
                         }
-                    );
-                }
-            }
+                    }
+                )
         );
         return defects;
     }
@@ -75,6 +66,7 @@ final class LtIncorrectNumberOfAttrs implements Lint<Map<String, XML>> {
 
     /**
      * Build object definitions.
+     *
      * @param pkg Package to scan
      * @return Map of object name and attributes count
      */
