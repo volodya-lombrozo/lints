932ffa7146e4aaa49264a9dcd4941ce6f3df3d46
h1alexbel <h1alexbelx@gmail.com>
2025-05-07 15:51:42 +0200

    feat(#376): conflict

--- src/test/java/benchmarks/SourceBench.java
+++ src/test/java/benchmarks/SourceBench.java
@@ -41,6 +41,9 @@ public class SourceBench {
     /**
      * Benchmark for XMIR scanning.
      * @param state State
+     * @todo #555:35min Enable `duplicate-names-in-diff-context` benchmark.
+     *  Currently, its slow, especially for `L` and `XL`-sized sources. Let's optimize it,
+     *  in order to enable this benchmark.
      * @todo #376:60min Enable redundant object in the single XMIR scope benchmarks.
      *  As for now, the lint is too slow, especially on L, XL and XXL-sized XMIRs.
      *  This happens mostly because of multiple XPath `//o` selects in the XSL. Once,
@@ -48,7 +51,8 @@ public class SourceBench {
      */
     @Benchmark
     public final void scansXmir(final BenchmarkState state) {
-        new Source(state.xmir).without("redundant-object").defects();
+        new Source(state.xmir).without("redundant-object", "duplicate-names-in-diff-context")
+            .defects();
     }
 
     /**
