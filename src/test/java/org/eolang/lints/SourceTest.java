16b8d74adf37598079128d41b166bea6587f6dbf
ext11n7 <w3bhunter11@gmail.com>
2026-07-04 12:19:21 +0300

    refactor(#1003): satisfy qulice in source test

--- src/test/java/org/eolang/lints/SourceTest.java
+++ src/test/java/org/eolang/lints/SourceTest.java
@@ -78,7 +78,7 @@ final class SourceTest {
             "defects found even though the code is clean",
             new Source(
                 new EoProgram("org/eolang/lints/valid-source.eo").parse()
-            ).without("mandatory-architect").defects(),
+            ).without(SourceTest.mandatoryArchitect()).defects(),
             Matchers.emptyIterable()
         );
     }
@@ -202,7 +202,7 @@ final class SourceTest {
             "mandatory-package",
             "comment-too-short",
             "mandatory-spdx",
-            "mandatory-architect",
+            SourceTest.mandatoryArchitect(),
             "no-attribute-formation",
             "unit-test-missing"
         );
@@ -221,7 +221,7 @@ final class SourceTest {
     void returnsOnlyOneDefect() {
         final Collection<Defect> defects = new Source(
             new EoProgram("org/eolang/lints/main-with-test.eo").parse()
-        ).without("mandatory-spdx", "mandatory-architect").defects();
+        ).without("mandatory-spdx", SourceTest.mandatoryArchitect()).defects();
         MatcherAssert.assertThat(
             String.format(
                 "Only one defect should be found, but got %d: %s",
@@ -245,7 +245,7 @@ final class SourceTest {
                 "empty-object",
                 "mandatory-package",
                 "mandatory-spdx",
-                "mandatory-architect",
+                SourceTest.mandatoryArchitect(),
                 "comment-too-short",
                 "no-attribute-formation",
                 "unit-test-missing"
@@ -282,7 +282,7 @@ final class SourceTest {
                 "empty-object",
                 "mandatory-package",
                 "mandatory-spdx",
-                "mandatory-architect",
+                SourceTest.mandatoryArchitect(),
                 "comment-too-short",
                 "no-attribute-formation",
                 "unit-test-missing"
@@ -386,6 +386,14 @@ final class SourceTest {
         ).accept(visitor, 0);
         final int lines = visitor.total();
         return lines >= src.minAllowed() && lines <= src.maxAllowed();
+    }
+
+    /**
+     * Mandatory architect lint name.
+     * @return Lint name
+     */
+    private static String mandatoryArchitect() {
+        return "mandatory-architect";
     }
 
     /**
